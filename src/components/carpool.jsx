4b3b28ee89e0db09e6a774fcc9fc50ad2ff7348b
Mel Marie Resurreccion <melmarieresurreccion@Mels-MacBook-Air.local>
2023-10-14 20:22:45 +0800

    push ui carpool

--- src/components/carpool.jsx
+++ src/components/carpool.jsx
@@ -8,8 +8,7 @@ import NavBar from './common/navbar'
 import './../css/carpool.css'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { geocodeByPlaceId, getLatLng } from 'react-google-places-autocomplete';
-
-
+import { MDBIcon } from 'mdb-react-ui-kit';
 
 const Search = () => {                            
   const [fromLocation, setFromLocation] = useState(null);
@@ -76,9 +75,77 @@ const Search = () => {
                 </div>
               </div>
             </div>
-            
-            
-           
+        </div>
+        <div className="container-fluid">
+          <div className="row bg-light p-3 mt-4 mx-2">
+            <div className="col-md-1">
+              <MDBIcon fas icon="car-side" size="4x" className="pt-4" />
+            </div>
+            <div className="col-md-2">
+              <p><b>Driver:</b> Ryan Velasquez</p>
+              <p><b>Type:</b> Sedan</p>
+              <p><b>Vehicle:</b> NG0 1354 Toyota Raze</p>
+              <p><b>Available Seat:</b> 3</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going from</h3> Capitol Commons Estancia Estancia Mall, Camino Drive, Pasig</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going to</h3>SM City Batangas Brgy, M.Pastor Ave, Village, Batangas</p>
+            </div>
+            <div className="col-md-2">
+              <p><h3>Date and time</h3>October 24, 2023 4:30 PM</p>
+            </div>
+            <div className="col-md-1 mt-4">
+              <button onClick={getLatLong} type="button" class="btn btn-lg btn-primary rounded button-find-ride">Book</button>
+            </div>
+          </div>
+          <div className="row bg-light p-3 mt-4 mx-2">
+            <div className="col-md-1">
+              <MDBIcon fas icon="shuttle-van" size="4x" className="pt-4" />
+            </div>
+            <div className="col-md-2">
+              <p><b>Driver:</b> Mel Marie</p>
+              <p><b>Type:</b> Van</p>
+              <p><b>Vehicle:</b> NG0 3334 Toyota Hiace</p>
+              <p><b>Available Seat:</b> 3</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going from</h3> Capitol Commons Estancia Estancia Mall, Camino Drive, Pasig</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going to</h3>SM City Batangas Brgy, M.Pastor Ave, Village, Batangas</p>
+            </div>
+            <div className="col-md-2">
+              <p><h3>Date and time</h3>October 24, 2023 3:30 PM</p>
+            </div>
+            <div className="col-md-1 mt-4">
+              <button onClick={getLatLong} type="button" class="btn btn-lg btn-primary rounded button-find-ride">Book</button>
+            </div>
+          </div>
+          <div className="row bg-light p-3 mt-4 mx-2">
+            <div className="col-md-1">
+              <MDBIcon fas icon="motorcycle"  size="4x" className="pt-4"/>
+            </div>
+            <div className="col-md-2">
+              <p><b>Driver:</b> Mel Marie</p>
+              <p><b>Type:</b> Motorcycle</p>
+              <p><b>Vehicle:</b> BMW 1000 CC</p>
+              <p><b>Available Seat:</b> 1</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going from</h3> Capitol Commons Estancia Estancia Mall, Camino Drive, Pasig</p>
+            </div>
+            <div className="col-md-3">
+              <p><h3>Going to</h3>SM City Batangas Brgy, M.Pastor Ave, Village, Batangas</p>
+            </div>
+            <div className="col-md-2">
+              <p><h3>Date and time</h3>October 24, 2023, 5:30 PM</p>
+            </div>
+            <div className="col-md-1 mt-4">
+              <button onClick={getLatLong} type="button" class="btn btn-lg btn-primary rounded button-find-ride">Book</button>
+            </div>
+          </div>
         </div>
     </>
   )
